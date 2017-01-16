b72c36c81cd749cc6bbc3339f2c397ff2cbd5777
emilycroft <emilyhcroft@gmail.com>
2017-01-16 09:52:12 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var result = "The line is currently: ", arr = []
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for ( let i = 0; i < katzDeliLine.length; i++ )
+     arr.push(`${i + 1}. ${katzDeliLine[i]}`)
+  }
+  return result + arr.join(", ")
+}
